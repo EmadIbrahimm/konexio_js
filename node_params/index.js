8d7b98810d4a1fd1fba0de0e7ba33b8ea047397b
Emad <emad.ibrahim533@gmail.com>
2020-06-15 18:25:27 +0200

    first commit

---
+++ node_params/index.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+    console.log('Nice to meet you ' + name);
+}
+sayHello('to my little friend');
+
+var firstname = process.argv[2];
+
+sayHello(firstname);
+console.log(typeof firstname)
+
---
+++ node_params/rot13.js
@@ -0,0 +1,16 @@
+
+//function rot13(str){
+
+// letters = "abc"
+// letters = rot13
+// str.charCodeAt()
+
+// } 
+// console.log(rot13)
+
+
+function rot13(str) {
+    console.log('abc');
+    console.log(String.fromCharCode)
+}
+console.log(process.argv)
